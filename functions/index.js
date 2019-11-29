4f9bb23dac38f9ef2e9491e97b1733a2cd81337a
kidow <wcgo2ling@hanmail.net>
2019-11-30 01:36:15 +0900

    Initial Deploy

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+const functions = require("firebase-functions");
+const { Nuxt } = require("nuxt-start");
+const express = require("express");
+
+const app = express();
+
+const nuxtConfig = require("./nuxt.config.js");
+
+const config = {
+  ...nuxtConfig,
+  dev: false,
+  debug: false,
+  buildDir: "nuxt"
+};
+const nuxt = new Nuxt(config);
+
+function handleRequest(req, res) {
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  return new Promise((resolve, reject) => {
+    nuxt.ready().then(() => {
+      nuxt.render(req, res, promise => {
+        promise.then(resolve).catch(reject);
+      });
+    });
+  });
+}
+
+app.use(handleRequest);
+exports.ssrapp = functions.https.onRequest(app);
